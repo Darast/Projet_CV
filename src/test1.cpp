5231b7f9e5f188033ebdb12176aa1e51d00a429c
MICK Sebastien <smick@fallieres.pedago.ipb.fr>
2015-12-04 10:30:07 +0100

    Nouveau procédé de morpho math

--- src/test1.cpp
+++ src/test1.cpp
@@ -12,7 +12,7 @@ using namespace cv;
 using namespace std;
 
 
-void process(const char* imsname, const int thmin, const int thmax){
+void process(const char* imsname, const char* imdname){
   Mat ims = imread(imsname, CV_LOAD_IMAGE_COLOR), HSV; // Read the given image in color
 
   struct timeval t0, tf;
@@ -21,35 +21,30 @@ void process(const char* imsname, const int thmin, const int thmax){
 
   //imshow("Original image", ims);
   cvtColor(ims, HSV, CV_BGR2HSV); // Convert to HSV color space: [0,180]x[0,255]x[0,255]
+  int iLowH = 23, iHighH = 63;    // Intervals used to threshold the channels
+  int iLowS = 38, iHighS = 255;
+  int iLowV = 25, iHighV = 133;
   
-  Mat HSVchannels[3];
-  split(HSV, HSVchannels); // Split channels
+  Mat imth;
+  inRange(HSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imth);
 
-  Mat H = HSVchannels[0]; // Extract H channel and apply thresholds
-  Mat Hmin, Hmax, Hth;
-  threshold(H, Hmin, thmin / 2, 255, THRESH_BINARY);
-  //imshow("Hmin", Hmin);
-  threshold(H, Hmax, thmax / 2, 255, THRESH_BINARY_INV);
-  //imshow("Hmax", Hmax);
-
-  Hth = (Hmax & Hmin);
-  //imshow("Thresholded hue before morph", Hth);
-
-  Mat seO = getStructuringElement(MORPH_RECT, Size(5, 5));
-  morphologyEx(Hth, Hth, MORPH_OPEN, seO, Point(-1, -1), 1, BORDER_CONSTANT, Scalar(0));
-  morphologyEx(Hth, Hth, MORPH_CLOSE, seO, Point(-1, -1), 1, BORDER_CONSTANT, Scalar(0));
-  imshow("Thresholded hue after morph", Hth);
+  Mat ell7 = getStructuringElement(MORPH_ELLIPSE, Size(7, 7));
+  Mat ell5 = getStructuringElement(MORPH_ELLIPSE, Size(5, 5));
+  erode(imth, imth, ell7);
+  dilate(imth, imth, ell5);
+  morphologyEx(imth, imth, MORPH_CLOSE, ell5, Point(-1, -1), 1, BORDER_CONSTANT, Scalar(0));
+  imshow("Thresholded hue after morph", imth);
 
   //* @HULL
   vector<vector<Point> > contours;
   vector<Vec4i> hierarchy;
 
-  findContours( Hth, contours, hierarchy, CV_RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0, 0) );  
+  findContours( imth, contours, hierarchy, CV_RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0, 0) );  
   cout << contours.size() << " contours have been identified." << endl;
 
   double ath = ims.cols * ims.rows / 4;
   vector< vector<Point> > hulls(contours.size());
-  Mat mask = Mat::zeros( Hth.size(), CV_8UC1 );
+  Mat mask = Mat::zeros( imth.size(), CV_8UC1 );
   Scalar color = Scalar(255);
 
   for( size_t i = 0; i < contours.size(); i++ ){
@@ -68,20 +63,22 @@ void process(const char* imsname, const int thmin, const int thmax){
   //* @SHOW
   imshow("Original image", ims);
   //imshow("Mask", mask );
-  Mat imth;
-  ims.copyTo(imth, mask);
-  imshow("Extracted part of the image", imth);
+  Mat imcut;
+  ims.copyTo(imcut, mask);
+  imshow("Extracted part of the image", imcut);
+
+  imwrite(imdname, mask);
   // @SHOW */
   waitKey();
 }
 
 void usage(const char* s){
-	cerr<<"Usage: " << s << " ims-name th-min th-max\n" << endl;
+	cerr<<"Usage: " << s << " ims-name imd-name\n" << endl;
 	exit(EXIT_FAILURE);
 }
 
 
-#define param 3
+#define param 2
 
 int main(int argc,const char* argv[]){
 	
@@ -89,7 +86,7 @@ int main(int argc,const char* argv[]){
 	{
 		usage(argv[0]);
 	}
-	process(argv[1], atoi(argv[2]), atoi(argv[3]));
+	process(argv[1], argv[2]);
 
 	return EXIT_SUCCESS;
 }
